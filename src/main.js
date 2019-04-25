7625bd51ed8cf1b2d5795b68d152d5c0d88dcb7a
bundai223 <bundai223@gmail.com>
2019-04-25 15:20:43 +0900

    vue analytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueAnalytics from 'vue-analytics'
+
 import App from './App'
 import router from './router'
 import storeroot from './store'
@@ -9,6 +11,10 @@ import storeroot from './store'
 Vue.config.productionTip = false
 
 Vue.use(Vuex)
+Vue.use(VueAnalytics, {
+  id: 'UA-139063243-2',
+  router
+})
 
 const store = new Vuex.Store(storeroot)
 
